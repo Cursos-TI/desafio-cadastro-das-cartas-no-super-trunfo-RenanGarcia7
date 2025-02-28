1d4804ad4432d523fa3e69cb975775dc0a7c1af9
RenanGarcia7 <renangarcia9@hotmail.com>
2025-02-28 20:11:26 +0000

    Desafio Super Trunfo Iniciante - Erros Corrigidos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,22 +13,22 @@
             printf("\nTema 1 - Cadastro das Cartas\n"); // Tema 1 - Cadastro das Cartas
              
                 printf("\nDigite o Código da Cidade: \n"); // Solicita ao usuário que digite o código da cidade
-                scanf("%s", codigodacidade); //armazena o codigo da cidade digitado pelo usuário
+                scanf("%s", codigodacidade); //Armazena o codigo da cidade digitado pelo usuário
 
                 printf("\nDigite o Nome da Cidade: \n"); // Solicita ao usuário que digite o nome da cidade
-                scanf("%s", nome); //armazena o nome da cidade digitado pelo usuário
+                scanf("%s", nome); //Armazena o nome da cidade digitado pelo usuário
 
                 printf("\nDigite a População da Cidade: \n"); // Solicita ao usuário que digite a população da cidade
-                scanf("%d", &populacao); //armazena a população da cidade digitado pelo usuário
+                scanf("%d", &populacao); //Armazena a população da cidade digitado pelo usuário
         
                 printf("\nDigite a Área da Cidade em km: \n"); // Solicita ao usuário que digite a área da cidade
-                scanf("%f", &area); //armazena a área da cidade digitado pelo usuário
+                scanf("%f", &area); //Armazena a área da cidade digitado pelo usuário
 
                 printf("\nDigite o PIB da Cidade: \n"); // Solicita ao usuário que digite o pib da cidade
-                scanf("%f", &pib); //armazena o pib da cidade digitado pelo usuário
+                scanf("%f", &pib); //Armazena o pib da cidade digitado pelo usuário
 
                 printf("\nDigite o número de Pontos Turísticos da Cidade: \n"); // Solicita ao usuário que digite a quantidade de pontos turísticos da cidade
-                scanf("%d", &numeropontoturistico); //armazena a quantidade de pontos turísticos da cidade digitado pelo usuário
+                scanf("%d", &numeropontoturistico); //Armazena a quantidade de pontos turísticos da cidade digitado pelo usuário
 
                     printf("\nOs dados da carta são: \n"); //Este bloco exibe para o usuário os dados da carta cadastrada
                     printf("Código: %s\n", codigodacidade);

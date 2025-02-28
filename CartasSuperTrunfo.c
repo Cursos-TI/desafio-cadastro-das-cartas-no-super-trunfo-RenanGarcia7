c7886ed1d7c92a09e05b7ab8770ad9e9d4863658
RenanGarcia7 <renangarcia9@hotmail.com>
2025-02-28 22:24:25 +0000

    Adicionado pib e densidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,16 +2,26 @@
 
             int main() {
     
+            
+            char estado[1]; // Variável do tipo Char para armazenar o Código da Carta da Cidade
             char codigodacidade[3]; // Variável do tipo Char para armazenar o Código da Carta da Cidade
             char nome[10]; // Variável do tipo Char para armazenar o Nome da Cidade
             int populacao; // Variável do tipo Int para armazenar a População da Cidade
             float area; // Variável do tipo Float para armazenar a Área da Cidade em Km
             float pib; // Variável do tipo Float para armazenar o PIB da Cidade
             int numeropontoturistico; // Variável do tipo Int para armazenar a Quantidade de Pontos Turísticos da Cidade
+            float densidadepopulacional; // Variável do tipo Float para armazenar a Densidade Populacional da cidade
+            float pibpercapta; // Variável do tipo Float para armazenar o PIB Per Capta da Cidade
+
 
             printf("Desafio Super Trunfo - Países\n"); // Desafio Super Trunfo - Países
             printf("\nTema 1 - Cadastro das Cartas\n"); // Tema 1 - Cadastro das Cartas
              
+            printf("\n Carta 1: \n"); //Carta 1
+
+                printf("\nDigite o Estado da Cidade: \n"); // Solicita ao usuário que digite o Estado da cidade
+                scanf("%s", estado); //Armazena o codigo da cidade digitado pelo usuário
+
                 printf("\nDigite o Código da Cidade: \n"); // Solicita ao usuário que digite o código da cidade
                 scanf("%s", codigodacidade); //Armazena o codigo da cidade digitado pelo usuário
 
@@ -30,13 +40,19 @@
                 printf("\nDigite o número de Pontos Turísticos da Cidade: \n"); // Solicita ao usuário que digite a quantidade de pontos turísticos da cidade
                 scanf("%d", &numeropontoturistico); //Armazena a quantidade de pontos turísticos da cidade digitado pelo usuário
 
-                    printf("\nOs dados da carta são: \n"); //Este bloco exibe para o usuário os dados da carta cadastrada
-                    printf("Código: %s\n", codigodacidade);
-                    printf("Nome: %s\n", nome);
-                    printf("População: %d\n", populacao);
-                    printf("Área: %f\n", area);
-                    printf("PIB: %f\n", pib);
-                    printf("Pontos Turísticos: %d\n", numeropontoturistico);
+                    densidadepopulacional = populacao / area;
+                    pibpercapta = pib / populacao;
+
+                        printf("\nOs dados da carta são: \n"); //Este bloco exibe para o usuário os dados da carta cadastrada
+                        printf("Estado: %s\n", estado);
+                        printf("Código: %s\n", codigodacidade);
+                        printf("Nome: %s\n", nome);
+                        printf("População: %d\n", populacao);
+                        printf("Área: %f\n", area);
+                        printf("PIB: %f\n", pib);
+                        printf("Pontos Turísticos: %d\n", numeropontoturistico);
+                        printf("Densidade Populacional: %.2f\n", densidadepopulacional);
+                        printf("PIB Per Capta: %.2f\n", pibpercapta);
         
         return 0;
         }
